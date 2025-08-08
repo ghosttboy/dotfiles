85fd7b81130446db8ca0a630badabb127e229591
ghostino <107803920+ghosttino@users.noreply.github.com>
2025-08-08 23:45:12 +0100

    fix(oil.nvim): show hidden files and fix navigation keymaps

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,3 +1,12 @@
-vim.pack.add({"https://github.com/stevearc/oil.nvim"})
-require("oil").setup()
+vim.pack.add({ "https://github.com/stevearc/oil.nvim" })
+require("oil").setup({
+  keymaps = {
+    ["l"] = { "actions.select", mode = "n" },
+    ["h"] = { "actions.parent", mode = "n" },
+  },
+  view_options = {
+    show_hidden = true,
+  },
+})
+
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
